09bab575e3a68e16d9ed46f06197b74111b43051
weaselflink <stefan@bissell.de>
2020-06-05 22:25:01 +0200

    add more three.js parts

--- client/src/main/kotlin/three/scenes/Scene.kt
+++ client/src/main/kotlin/three/scenes/Scene.kt
@@ -3,8 +3,11 @@
 package three.scenes
 
 import three.core.Object3D
+import three.materials.Material
 
 open external class Scene : Object3D {
 
     var background: dynamic
+
+    var overrideMaterial: Material
 }
