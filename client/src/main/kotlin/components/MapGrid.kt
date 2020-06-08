eea6346163ccdc06ed49e7b4a6bc507264a19acf
weaselflink <stefan@bissell.de>
2020-06-08 12:28:42 +0200

    optimize map grid drawing

--- client/src/main/kotlin/components/MapGrid.kt
+++ client/src/main/kotlin/components/MapGrid.kt
@@ -3,15 +3,13 @@ package components
 import CanvasDimensions
 import de.bissell.starcruiser.Vector2
 import dimensions
-import lineTo
-import moveTo
 import org.w3c.dom.CanvasRenderingContext2D
 import org.w3c.dom.HTMLCanvasElement
 import translateToCenter
 
 class MapGrid(
     private val canvas: HTMLCanvasElement,
-    private val majorGap: Double = 1000.0
+    private val majorGridSize: Double = 1000.0
 ) {
 
     private val ctx = canvas.getContext(contextId = "2d")!! as CanvasRenderingContext2D
@@ -27,23 +25,59 @@ class MapGrid(
         save()
         translateToCenter()
         strokeStyle = "#4682b4"
-        (-20..20).forEach { gridX ->
-            beginPath()
-            moveTo(Vector2(gridX * majorGap, -20_000.0).adjustForMap(center, scale))
-            lineTo(Vector2(gridX * majorGap, +20_000.0).adjustForMap(center, scale))
-            stroke()
+
+        visibleGridSquares(center, scale).forEach {
+            drawSquare(it, center, scale)
         }
-        (-20..20).forEach { gridY ->
-            beginPath()
-            moveTo(Vector2(-20_000.0, gridY * majorGap).adjustForMap(center, scale))
-            lineTo(Vector2(20_000.0, gridY * majorGap).adjustForMap(center, scale))
-            stroke()
-        }
+
         restore()
+    }
+
+    private fun CanvasRenderingContext2D.drawSquare(gridSquare: GridSquare, center: Vector2, scale: Double) {
+        gridSquare.topLeft.adjustForMap(center, scale).let {
+            strokeRect(it.x, it.y, majorGridSize.adjustForMap(scale), majorGridSize.adjustForMap(scale))
+        }
     }
 
     private fun Vector2.adjustForMap(center: Vector2, scale: Double) =
         ((this - center) * scale).let { Vector2(it.x, -it.y) }
 
     private fun Double.adjustForMap(scale: Double) = this * scale
+
+    private fun visibleGridSquares(center: Vector2, scale: Double): List<GridSquare> {
+        val canvasCenter = Vector2(dim.width * 0.5, dim.height * 0.5)
+        val centerX = 50
+        val centerY = 13
+
+        return (0..100).flatMap { xIndex ->
+            (0..25).map { yIndex ->
+                val topLeft = Vector2((xIndex - centerX) * majorGridSize, -(yIndex - centerY) * majorGridSize)
+                val bottomRight = topLeft + Vector2(majorGridSize, -majorGridSize)
+
+                GridSquare(
+                    xIndex,
+                    yIndex,
+                    topLeft,
+                    bottomRight
+                )
+            }.filter { square ->
+                val topLeft = square.topLeft.adjustForMap(center, scale) + canvasCenter
+                val bottomRight = square.bottomRight.adjustForMap(center, scale) + canvasCenter
+
+                if (square.xIndex == centerX && square.yIndex == centerY) {
+                    println(square)
+                    println("$topLeft $bottomRight")
+                }
+
+                topLeft.x <= dim.width && bottomRight.x >= 0.0 && topLeft.y <= dim.height && bottomRight.y >= 0.0
+            }
+        }
+    }
 }
+
+private data class GridSquare(
+    val xIndex: Int,
+    val yIndex: Int,
+    val topLeft: Vector2,
+    val bottomRight: Vector2
+)
