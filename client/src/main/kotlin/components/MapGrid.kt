0cbbfd572956ad8f2314acd357eaf204f70bc095
weaselflink <stefan@bissell.de>
2020-06-08 15:20:43 +0200

    clean code

--- client/src/main/kotlin/components/MapGrid.kt
+++ client/src/main/kotlin/components/MapGrid.kt
@@ -15,48 +15,64 @@ import translateToCenter
 
 class MapGrid(
     private val canvas: HTMLCanvasElement,
-    private val majorGridSize: Double = 1000.0
+    private val majorGridSize: Double = 1000.0,
+    private val centerIndexX: Int = 50,
+    private val centerIndexY: Int = 13
 ) {
 
     private val ctx = canvas.getContext(contextId = "2d")!! as CanvasRenderingContext2D
-    private var dim = CanvasDimensions(100, 100)
 
     fun draw(center: Vector2, scale: Double) {
-        dim = canvas.dimensions()
-
-        ctx.draw(center, scale)
+        MapGridRenderer(canvas.dimensions(), center, scale).render()
     }
 
-    private fun CanvasRenderingContext2D.draw(center: Vector2, scale: Double) {
-        save()
-        translateToCenter()
-        lineWidth = dim.vmin * 0.3
-        strokeStyle = "#1d3549"
-        fillStyle = "#664400"
-        val textSize = 100.0.adjustForMap(scale).toInt()
-        font = "bold ${textSize.px} sans-serif"
-        textBaseline = CanvasTextBaseline.TOP
-        textAlign = CanvasTextAlign.LEFT
+    private inner class MapGridRenderer(
+        private val dim: CanvasDimensions,
+        private val center: Vector2,
+        private val scale: Double
+    ) {
 
-        visibleGridSquares(center, scale).forEach {
-            drawSquare(it, center, scale)
+        fun render() {
+            ctx.draw()
         }
 
-        restore()
-    }
-
-    private fun CanvasRenderingContext2D.drawSquare(gridSquare: GridSquare, center: Vector2, scale: Double) {
-        gridSquare.topLeft.adjustForMap(center, scale).let {
-            strokeRect(it.x, it.y, majorGridSize.adjustForMap(scale), majorGridSize.adjustForMap(scale))
-
+        private fun CanvasRenderingContext2D.draw() {
             save()
-            if (majorGridSize * scale > dim.vmin * 10) {
-                val gap = 50.0.adjustForMap(scale).toInt()
-                fillStyle = "#332200"
-                fillText(gridSquare.label(), it.x + gap, it.y + gap)
-            }
-            restore()
+            translateToCenter()
+            lineWidth = dim.vmin * 0.3
+            strokeStyle = "#1d3549"
+            fillStyle = "#664400"
+            val textSize = 100.0.adjustForMap().toInt()
+            font = "bold ${textSize.px} sans-serif"
+            textBaseline = CanvasTextBaseline.TOP
+            textAlign = CanvasTextAlign.LEFT
 
+            visibleGridSquares().forEach {
+                drawSquare(it)
+            }
+
+            restore()
+        }
+
+        private fun CanvasRenderingContext2D.drawSquare(gridSquare: GridSquare) {
+            gridSquare.topLeftAdjusted.let {
+                strokeRect(it.x, it.y, majorGridSize.adjustForMap(), majorGridSize.adjustForMap())
+                drawGridLabel(gridSquare)
+                drawMinorGrid(it)
+            }
+        }
+
+        private fun CanvasRenderingContext2D.drawGridLabel(gridSquare: GridSquare) {
+            if (majorGridSize * scale > dim.vmin * 10) {
+                save()
+                val gap = 50.0.adjustForMap().toInt()
+                fillStyle = "#332200"
+                fillText(gridSquare.label(), gridSquare.topLeftAdjusted.x + gap, gridSquare.topLeftAdjusted.y + gap)
+                restore()
+            }
+        }
+
+        private fun CanvasRenderingContext2D.drawMinorGrid(it: Vector2) {
             if (majorGridSize * scale > dim.vmin * 40) {
                 (1..9).map { minorX ->
                     (1..9).map { minorY ->
@@ -69,45 +85,50 @@ class MapGrid(
                 }
             }
         }
-    }
 
-    private fun Vector2.adjustForMap(center: Vector2, scale: Double) =
-        ((this - center) * scale).let { Vector2(it.x, -it.y) }
+        private fun Vector2.adjustForMap() =
+            ((this - center) * scale).let { Vector2(it.x, -it.y) }
 
-    private fun Double.adjustForMap(scale: Double) = this * scale
+        private fun Double.adjustForMap() = this * scale
 
-    private fun visibleGridSquares(center: Vector2, scale: Double): List<GridSquare> {
-        val canvasCenter = Vector2(dim.width * 0.5, dim.height * 0.5)
-        val centerX = 50
-        val centerY = 13
+        private fun visibleGridSquares(): List<GridSquare> {
+            val canvasCenter = Vector2(dim.width * 0.5, dim.height * 0.5)
 
-        return (0..99).flatMap { xIndex ->
-            (0..25).map { yIndex ->
-                val topLeft = Vector2((xIndex - centerX) * majorGridSize, -(yIndex - centerY) * majorGridSize)
-                val bottomRight = topLeft + Vector2(majorGridSize, -majorGridSize)
+            return (0..99).flatMap { xIndex ->
+                (0..25).map { yIndex ->
+                    val topLeft = Vector2(
+                        (xIndex - centerIndexX) * majorGridSize,
+                        -(yIndex - centerIndexY) * majorGridSize
+                    )
+                    val bottomRight = topLeft + Vector2(majorGridSize, -majorGridSize)
 
-                GridSquare(
-                    xIndex,
-                    yIndex,
-                    topLeft,
-                    bottomRight
-                )
-            }.filter { square ->
-                val topLeft = square.topLeft.adjustForMap(center, scale) + canvasCenter
-                val bottomRight = square.bottomRight.adjustForMap(center, scale) + canvasCenter
+                    GridSquare(
+                        xIndex,
+                        yIndex,
+                        topLeft,
+                        topLeft.adjustForMap(),
+                        bottomRight,
+                        bottomRight.adjustForMap()
+                    )
+                }.filter { square ->
+                    val topLeft = square.topLeftAdjusted + canvasCenter
+                    val bottomRight = square.bottomRightAdjusted + canvasCenter
 
-                topLeft.x <= dim.width && bottomRight.x >= 0.0 && topLeft.y <= dim.height && bottomRight.y >= 0.0
+                    topLeft.x <= dim.width && bottomRight.x >= 0.0 && topLeft.y <= dim.height && bottomRight.y >= 0.0
+                }
             }
         }
     }
-}
 
-private data class GridSquare(
-    val xIndex: Int,
-    val yIndex: Int,
-    val topLeft: Vector2,
-    val bottomRight: Vector2
-) {
+    private data class GridSquare(
+        val xIndex: Int,
+        val yIndex: Int,
+        val topLeft: Vector2,
+        val topLeftAdjusted: Vector2,
+        val bottomRight: Vector2,
+        val bottomRightAdjusted: Vector2
+    ) {
 
-    fun label() = "${'A' + yIndex}${xIndex.pad(2)}"
+        fun label() = "${'A' + yIndex}${xIndex.pad(2)}"
+    }
 }
