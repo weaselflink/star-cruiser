1cb51ebdceb3a1c8a2b44e90bda0992a0ca8188c
weaselflink <stefan@bissell.de>
2020-06-11 12:05:40 +0200

    add test for websocket throttle (first actor test)

---
+++ server/src/test/kotlin/de/bissell/starcruiser/client/ThrottleActorTest.kt
@@ -0,0 +1,70 @@
+package de.bissell.starcruiser.client
+
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+import kotlin.random.Random
+
+class ThrottleActorTest {
+
+    @Test
+    fun `records inflight messages`() {
+        runActorTest {
+            send(ThrottleMessage.AddInflightMessage(CompletableDeferred()))
+            send(ThrottleMessage.AddInflightMessage(CompletableDeferred()))
+            send(ThrottleMessage.AddInflightMessage(CompletableDeferred()))
+
+            val result = CompletableDeferred<Int>()
+            send(ThrottleMessage.GetInflightMessageCount(result))
+
+            expectThat(result.await()).isEqualTo(3)
+        }
+    }
+
+    @Test
+    fun `uses acknowledgement messages`() {
+        runActorTest {
+            val firstResult = CompletableDeferred<Long>()
+            send(ThrottleMessage.AddInflightMessage(firstResult))
+            send(ThrottleMessage.AddInflightMessage(CompletableDeferred()))
+            send(ThrottleMessage.AddInflightMessage(CompletableDeferred()))
+
+            send(ThrottleMessage.AcknowledgeInflightMessage(firstResult.await()))
+
+            val result = CompletableDeferred<Int>()
+            send(ThrottleMessage.GetInflightMessageCount(result))
+
+            expectThat(result.await()).isEqualTo(2)
+        }
+    }
+
+    @Test
+    fun `ignores acknowledgement messages with wrong id`() {
+        runActorTest {
+            send(ThrottleMessage.AddInflightMessage(CompletableDeferred()))
+            send(ThrottleMessage.AddInflightMessage(CompletableDeferred()))
+            send(ThrottleMessage.AddInflightMessage(CompletableDeferred()))
+
+            send(ThrottleMessage.AcknowledgeInflightMessage(Random.nextLong()))
+
+            val result = CompletableDeferred<Int>()
+            send(ThrottleMessage.GetInflightMessageCount(result))
+
+            expectThat(result.await()).isEqualTo(3)
+        }
+    }
+
+    private fun runActorTest(block: suspend SendChannel<ThrottleMessage>.() -> Unit) {
+        runBlocking {
+            val channel = createThrottleActor()
+            try {
+                channel.block()
+            } finally {
+                channel.close()
+            }
+        }
+    }
+}
