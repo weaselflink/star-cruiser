10a8d3d6e79745f9f547c9167ddb38add4619295
weaselflink <stefan@bissell.de>
2020-06-16 23:27:51 +0200

    use newest stable versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     kotlin("jvm") version "1.3.72" apply false
     kotlin("js") version "1.3.72" apply false
     kotlin("plugin.serialization") version "1.3.72" apply false
-    id("com.github.johnrengelman.shadow") version "5.0.0" apply false
+    id("com.github.johnrengelman.shadow") version "6.0.0" apply false
     id("com.github.ben-manes.versions") version "0.28.0" apply false
 }
 
