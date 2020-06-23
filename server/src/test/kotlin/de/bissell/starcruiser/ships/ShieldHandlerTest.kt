f7c0133961bb50833763de2be17f5b6f8bbf4bce
weaselflink <stefan@bissell.de>
2020-06-23 17:05:16 +0200

    add test for shield handler

---
+++ server/src/test/kotlin/de/bissell/starcruiser/ships/ShieldHandlerTest.kt
@@ -0,0 +1,121 @@
+package de.bissell.starcruiser.ships
+
+import de.bissell.starcruiser.GameTime
+import de.bissell.starcruiser.isNear
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+import strikt.assertions.isFalse
+import strikt.assertions.isTrue
+import java.time.Instant
+
+class ShieldHandlerTest {
+
+    private val time = GameTime().apply {
+        update(Instant.EPOCH)
+    }
+    private val shieldTemplate = ShieldTemplate()
+    private val shieldHandler = ShieldHandler(shieldTemplate)
+
+    @Test
+    fun `starts with full shields in up state`() {
+        expectThat(shieldHandler.toMessage())
+            .and {
+                get { up }.isTrue()
+                get { strength }.isNear(shieldTemplate.strength)
+            }
+    }
+
+    @Test
+    fun `applies damage to shields and reports no hull damage if shields up`() {
+        expectThat(shieldHandler.takeDamageAndReportHullDamage(3.0))
+            .isEqualTo(0.0)
+        expectThat(shieldHandler.toMessage().strength)
+            .isNear(shieldTemplate.strength - 3.0)
+    }
+
+    @Test
+    fun `shields are activated after taking damage since last update`() {
+        shieldHandler.takeDamageAndReportHullDamage(3.0)
+
+        stepTimeTo(0.1)
+
+        expectThat(shieldHandler.toMessage().activated)
+            .isTrue()
+
+        stepTimeTo(0.1)
+
+        expectThat(shieldHandler.toMessage().activated)
+            .isFalse()
+    }
+
+    @Test
+    fun `recharges shields`() {
+        shieldHandler.takeDamageAndReportHullDamage(3.0)
+
+        expectThat(shieldHandler.toMessage().strength)
+            .isNear(shieldTemplate.strength - 3.0)
+
+        stepTimeTo(5.0)
+
+        expectThat(shieldHandler.toMessage().strength)
+            .isNear(shieldTemplate.strength - 3.0 + shieldTemplate.rechargeSpeed * 5.0)
+    }
+
+    @Test
+    fun `reports damage to hull if shields down`() {
+        shieldHandler.setUp(false)
+
+        expectThat(shieldHandler.takeDamageAndReportHullDamage(3.0))
+            .isEqualTo(3.0)
+        expectThat(shieldHandler.toMessage().strength)
+            .isNear(shieldTemplate.strength)
+    }
+
+    @Test
+    fun `reports damage to hull if damage exceeds shields`() {
+        expectThat(shieldHandler.takeDamageAndReportHullDamage(shieldTemplate.strength + 3.0))
+            .isEqualTo(3.0)
+        expectThat(shieldHandler.toMessage().strength)
+            .isNear(0.0)
+    }
+
+    @Test
+    fun `shields go down if below failure strength`() {
+        shieldHandler.takeDamageAndReportHullDamage(shieldTemplate.strength - shieldTemplate.failureStrength + 3.0)
+
+        stepTimeTo(0.1)
+
+        expectThat(shieldHandler.toMessage().up)
+            .isFalse()
+    }
+
+    @Test
+    fun `shield cannot go up if below activation strength`() {
+        shieldHandler.takeDamageAndReportHullDamage(shieldTemplate.strength - shieldTemplate.activationStrength + 3.0)
+        shieldHandler.setUp(false)
+        shieldHandler.setUp(true)
+
+        expectThat(shieldHandler.toMessage().up)
+            .isFalse()
+    }
+
+    @Test
+    fun `shield can go up after reaching activation strength`() {
+        shieldHandler.takeDamageAndReportHullDamage(shieldTemplate.strength - shieldTemplate.activationStrength + 3.0)
+        shieldHandler.setUp(false)
+
+        stepTimeTo(4.0 / shieldTemplate.rechargeSpeed)
+
+        shieldHandler.setUp(true)
+
+        expectThat(shieldHandler.toMessage().up)
+            .isTrue()
+    }
+
+    private fun stepTimeTo(seconds: Number) {
+        time.update(Instant.EPOCH.plusMillis((seconds.toDouble() * 1000).toLong()))
+        shieldHandler.update(time)
+        shieldHandler.endUpdate()
+    }
+}
