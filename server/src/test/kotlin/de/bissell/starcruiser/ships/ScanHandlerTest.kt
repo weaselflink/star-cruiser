c87c6e122cde8f5742f46894c81f921a063b609b
weaselflink <stefan@bissell.de>
2020-06-23 11:55:31 +0200

    add test for scan handler

---
+++ server/src/test/kotlin/de/bissell/starcruiser/ships/ScanHandlerTest.kt
@@ -0,0 +1,62 @@
+package de.bissell.starcruiser.ships
+
+import de.bissell.starcruiser.GameTime
+import de.bissell.starcruiser.LockStatus
+import de.bissell.starcruiser.ObjectId
+import de.bissell.starcruiser.isNear
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isA
+import strikt.assertions.isEqualTo
+import strikt.assertions.isFalse
+import strikt.assertions.isTrue
+import java.time.Instant
+
+class ScanHandlerTest {
+
+    private val time = GameTime().apply {
+        update(Instant.EPOCH)
+    }
+    private val targetId = ObjectId.random()
+    private val scanningSpeed = 0.25
+    private val scanHandler = ScanHandler(targetId, scanningSpeed)
+
+    @Test
+    fun `starts with zero progress`() {
+        expectThat(scanHandler.isComplete).isFalse()
+        expectThat(scanHandler.toMessage())
+            .and {
+                get { targetId }.isEqualTo(targetId)
+                get { progress }.isNear(0.0)
+            }
+    }
+
+    @Test
+    fun `still in progress after insufficient time passed`() {
+        stepTimeTo(3)
+
+        expectThat(scanHandler.isComplete).isFalse()
+        expectThat(scanHandler.toMessage())
+            .and {
+                get { targetId }.isEqualTo(targetId)
+                get { progress }.isNear(0.75)
+            }
+    }
+
+    @Test
+    fun `locked after sufficient time passed`() {
+        stepTimeTo(5)
+
+        expectThat(scanHandler.isComplete).isTrue()
+        expectThat(scanHandler.toMessage())
+            .and {
+                get { targetId }.isEqualTo(targetId)
+                get { progress }.isNear(1.25)
+            }
+    }
+
+    private fun stepTimeTo(seconds: Number) {
+        time.update(Instant.EPOCH.plusMillis((seconds.toDouble() * 1000).toLong()))
+        scanHandler.update(time)
+    }
+}
