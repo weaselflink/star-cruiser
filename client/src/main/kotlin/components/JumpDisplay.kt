c6e7191dbd5c36557de3e1fa5edca7a413b57422
weaselflink <stefan@stefanbissell.de>
2020-06-30 10:56:14 +0200

    extract progress bar code

--- client/src/main/kotlin/components/JumpDisplay.kt
+++ client/src/main/kotlin/components/JumpDisplay.kt
@@ -1,107 +1,45 @@
 package components
 
 import CanvasDimensions
-import context2D
 import de.bissell.starcruiser.JumpDriveMessage
-import org.w3c.dom.*
-import px
+import org.w3c.dom.HTMLCanvasElement
 
 class JumpDisplay(
-    private val canvas: HTMLCanvasElement,
-    private val xExpr: (CanvasDimensions) -> Double,
-    private val yExpr: (CanvasDimensions) -> Double,
-    private val widthExpr: (CanvasDimensions) -> Double = { it.vmin * 34 },
-    private val heightExpr: (CanvasDimensions) -> Double = { it.vmin * 6 }
+    canvas: HTMLCanvasElement,
+    xExpr: (CanvasDimensions) -> Double,
+    yExpr: (CanvasDimensions) -> Double,
+    widthExpr: (CanvasDimensions) -> Double = { it.vmin * 34 },
+    heightExpr: (CanvasDimensions) -> Double = { it.vmin * 6 }
 ) {
 
-    private val ctx: CanvasRenderingContext2D = canvas.context2D
+    private val canvasProgress = CanvasProgress(
+        canvas = canvas,
+        xExpr = xExpr,
+        yExpr = yExpr,
+        widthExpr = widthExpr,
+        heightExpr = heightExpr,
+        backgroundColor = "#111",
+        foregroundColor = "#888"
+    )
 
     fun draw(jumpDriveMessage: JumpDriveMessage) {
-        val dim = currentDimensions(canvas)
-
-        with(ctx) {
-            save()
-
-            lineWidth = dim.lineWidth * 0.5
-            val textSize = (dim.height * 0.7).toInt()
-            font = "${textSize.px} sans-serif"
-
-            drawBackground(dim)
-            drawBorder(dim)
-            fillStyle = "#888"
-            textBaseline = CanvasTextBaseline.MIDDLE
-            drawStatusText(dim, jumpDriveMessage)
-            drawDistanceText(dim, jumpDriveMessage)
-            barWidth(jumpDriveMessage)?.also {
-                drawBar(dim, it)
-                clipBar(dim, it)
-                fillStyle = "#111"
-                drawStatusText(dim, jumpDriveMessage)
-                drawDistanceText(dim, jumpDriveMessage)
-            }
-
-
-            restore()
-        }
-    }
-
-    private fun CanvasRenderingContext2D.drawBackground(dim: ComponentDimensions) {
-        fillStyle = "#111"
-        fillRect(dim.bottomX, dim.bottomY, dim.width, -dim.height)
-    }
-
-    private fun CanvasRenderingContext2D.drawBorder(dim: ComponentDimensions) {
-        strokeStyle = "#888"
-        strokeRect(dim.bottomX, dim.bottomY, dim.width, -dim.height)
-    }
-
-    private fun CanvasRenderingContext2D.drawStatusText(dim: ComponentDimensions, jumpDriveMessage: JumpDriveMessage) {
-        val margin = marginExpr(dim)
-        val statusText = when (jumpDriveMessage) {
-            is JumpDriveMessage.Ready -> "Ready"
-            is JumpDriveMessage.Jumping -> "Jumping"
-            is JumpDriveMessage.Recharging -> "Charging"
-        }
-
-        textAlign = CanvasTextAlign.LEFT
-        fillText(statusText, dim.bottomX + margin, dim.bottomY - dim.height * 0.5)
-    }
-
-    private fun CanvasRenderingContext2D.drawDistanceText(dim: ComponentDimensions, jumpDriveMessage: JumpDriveMessage) {
-        val margin = marginExpr(dim)
-        val distanceText = jumpDriveMessage.distance.toString()
-
-        textAlign = CanvasTextAlign.RIGHT
-        fillText(distanceText, dim.bottomX + dim.width - margin, dim.bottomY - dim.height * 0.5)
-    }
-
-    private fun CanvasRenderingContext2D.drawBar(dim: ComponentDimensions, barWidth: Double) {
-        fillStyle = "#888"
-        drawBarRect(dim, barWidth)
-        fill()
-    }
-
-    private fun CanvasRenderingContext2D.clipBar(dim: ComponentDimensions, barWidth: Double) {
-        drawBarRect(dim, barWidth)
-        clip()
-    }
-
-    private fun CanvasRenderingContext2D.drawBarRect(dim: ComponentDimensions, barWidth: Double) {
-        beginPath()
-        rect(dim.bottomX, dim.bottomY, dim.width * barWidth, -dim.height)
-    }
-
-    private fun barWidth(jumpDriveMessage: JumpDriveMessage) =
         when (jumpDriveMessage) {
-            is JumpDriveMessage.Jumping -> jumpDriveMessage.progress
-            is JumpDriveMessage.Recharging -> jumpDriveMessage.progress
-            else -> null
+            is JumpDriveMessage.Ready -> {
+                canvasProgress.leftText = "Ready"
+                canvasProgress.progress = null
+            }
+            is JumpDriveMessage.Jumping -> {
+                canvasProgress.leftText = "Jumping"
+                canvasProgress.progress = jumpDriveMessage.progress
+            }
+            is JumpDriveMessage.Recharging -> {
+                canvasProgress.leftText = "Charging"
+                canvasProgress.progress = jumpDriveMessage.progress
+            }
         }
 
-    private fun marginExpr(dim: ComponentDimensions) = dim.height * 0.2
+        canvasProgress.rightText = jumpDriveMessage.distance.toString()
 
-    private fun currentDimensions(canvas: HTMLCanvasElement) =
-        ComponentDimensions.calculate(
-            canvas, xExpr, yExpr, widthExpr, heightExpr
-        )
+        canvasProgress.draw()
+    }
 }
