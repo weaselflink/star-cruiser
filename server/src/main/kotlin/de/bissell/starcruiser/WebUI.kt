f2b7cf44438935cfddac5d9cb835301ee94e9059
weaselflink <stefan@stefanbissell.de>
2020-07-07 09:17:30 +0200

    clean up CSS dsl

--- server/src/main/kotlin/de/bissell/starcruiser/WebUI.kt
+++ server/src/main/kotlin/de/bissell/starcruiser/WebUI.kt
@@ -1,5 +1,6 @@
 package de.bissell.starcruiser
 
+import io.ktor.application.ApplicationCall
 import io.ktor.application.call
 import io.ktor.html.respondHtml
 import io.ktor.http.ContentType
@@ -103,166 +104,41 @@ fun Routing.webUi() {
     }
 
     get("/css/client.css") {
-        call.respondText(contentType = ContentType.Text.CSS) {
-            CSSBuilder(indent = "\t").apply {
-                body {
-                    margin = 0.vmin.value
-                    padding = 0.vmin.value
-                    backgroundColor = Color("#222")
-                    color = white
-                    fontFamily = "sans-serif"
-                }
-                "::backdrop" {
-                    backgroundColor = Color("#222")
-                }
-                canvas {
-                    position = fixed
-                    top = 0.vmin
-                    left = 0.vmin
-                    width = 100.pct
-                    height = 100.pct
-                }
-                ".topLeftButtons, .topRightButtons" {
-                    position = fixed
-                    display = grid
-                    gap = Gap(1.vmin.value)
-                    marginTop = 1.vmin
-                    top = 0.vmin
-                    zIndex = 10
-                    padding(4.vmin, 0.vmin)
-                }
-                ".topLeftButtons" {
-                    left = 0.vmin
-                }
-                ".topRightButtons" {
-                    right = 0.vmin
-                }
-                ".spawn" {
-                    marginBottom = 2.vmin
-                }
-                ".playerShips" {
-                    display = grid
-                    gap = Gap(1.vmin.value)
-                    alignSelf = Align.flexStart
-                }
-                "button" {
-                    display = block
-                    color = lightGrey
-                    backgroundColor = black
-                    textAlign = TextAlign.left
-                    borderWidth = 0.5.vmin
-                    borderStyle = solid
-                    borderColor = darkGrey
-                    cursor = pointer
-                    fontWeight = bold
-                    fontSize = 3.vmin
-                    paddingTop = 1.vmin
-                    paddingBottom = 1.5.vmin
-                    outline = Outline.none
-                }
-                "button.current" {
-                    color = black
-                    backgroundColor = darkGrey
-                }
-                "button:hover" {
-                    color = black
-                    backgroundColor = dimGrey
-                }
-                "button.current:hover" {
-                    color = black
-                    backgroundColor = Color("#c3c3c3")
-                }
-                "button.leftEdge" {
-                    paddingRight = 3.vmin
-                    paddingLeft = 2.vmin
-                    borderTopLeftRadius = 0.vmin
-                    borderTopRightRadius = 4.vmin
-                    borderBottomRightRadius = 4.vmin
-                    borderBottomLeftRadius = 0.vmin
-                }
-                "button.rightEdge" {
-                    paddingTop = 1.vmin
-                    paddingRight = 2.vmin
-                    paddingBottom = 1.5.vmin
-                    paddingLeft = 3.vmin
-                    borderTopLeftRadius = 4.vmin
-                    borderTopRightRadius = 0.vmin
-                    borderBottomRightRadius = 0.vmin
-                    borderBottomLeftRadius = 4.vmin
-                }
-                "button:active" {
-                    color = black
-                    backgroundColor = dimGrey
-                    borderColor = dimGrey
-                }
-                "#selection-details" {
-                    position = fixed
-                    right = 0.vmin
-                    bottom = 0.vmin
-                    zIndex = 10
-                    display = grid
-                    gridTemplateColumns = GridTemplateColumns("auto 10vmin")
-                    gap = Gap("2vmin")
-                    padding(3.vmin, 6.vmin)
-                    color = lightGrey
-                    backgroundColor = black
-                    borderWidth = 0.5.vmin
-                    borderStyle = solid
-                    borderColor = darkGrey
-                    borderTopLeftRadius = 4.vmin
-                    fontWeight = bold
-                    fontSize = 3.vmin
-                }
-                "#selection-details button" {
-                    borderRadius = 4.vmin
-                    declarations["justifySelf"] = "center"
-                    paddingLeft = 4.vmin
-                    paddingRight = 4.vmin
-                }
-                "#selection-details .designation" {
-                    gridColumn = GridColumn("span 2")
-                }
-                "#selection-details .bearing" {
-                    textAlign = TextAlign.right
-                }
-                "#selection-details .range" {
-                    textAlign = TextAlign.right
-                }
-                "#selection-details .detailsScanButton" {
-                    gridRow = GridRow("4")
-                    gridColumn = GridColumn("1 / span 2")
-                }
-                "#selection-details .detailsDeleteButton" {
-                    gridRow = GridRow("4")
-                    gridColumn = GridColumn("1 / span 2")
-                }
-                "#destroyed-ui" {
-                    display = grid
-                    minHeight = 100.vh
-                    alignContent = Align.center
-                    justifyContent = JustifyContent.center
-                }
-                ".popup" {
-                    zIndex = 10
-                    display = grid
-                    gap = Gap("2vmin")
-                    padding(3.vmin, 6.vmin)
-                    color = lightGrey
-                    backgroundColor = black
-                    borderWidth = 0.5.vmin
-                    borderStyle = solid
-                    borderColor = darkGrey
-                    borderRadius = 4.vmin
-                    fontWeight = bold
-                    fontSize = 4.vmin
-                    justifyContent = JustifyContent.center
-                }
-                ".popup button" {
-                    borderRadius = 4.vmin
-                    paddingLeft = 4.vmin
-                    paddingRight = 4.vmin
-                }
-            }.toString()
+        call.respondCss {
+            body {
+                margin = 0.vmin.value
+                padding = 0.vmin.value
+                backgroundColor = Color("#222")
+                color = white
+                fontFamily = "sans-serif"
+            }
+            "::backdrop" {
+                backgroundColor = Color("#222")
+            }
+            canvas {
+                position = fixed
+                top = 0.vmin
+                left = 0.vmin
+                width = 100.pct
+                height = 100.pct
+            }
+            ".spawn" {
+                marginBottom = 2.vmin
+            }
+            ".playerShips" {
+                display = grid
+                gap = Gap(1.vmin.value)
+                alignSelf = Align.flexStart
+            }
+            "#destroyed-ui" {
+                display = grid
+                minHeight = 100.vh
+                alignContent = Align.center
+                justifyContent = JustifyContent.center
+            }
+            buttonCss()
+            selectionDetailsCss()
+            popupCss()
         }
     }
 
@@ -403,3 +279,142 @@ private fun BODY.mainScreenUi() {
         canvas {}
     }
 }
+
+private fun CSSBuilder.buttonCss() {
+    ".topLeftButtons, .topRightButtons" {
+        position = fixed
+        display = grid
+        gap = Gap(1.vmin.value)
+        marginTop = 1.vmin
+        top = 0.vmin
+        zIndex = 10
+        padding(4.vmin, 0.vmin)
+    }
+    ".topLeftButtons" {
+        left = 0.vmin
+    }
+    ".topRightButtons" {
+        right = 0.vmin
+    }
+    "button" {
+        display = block
+        color = lightGrey
+        backgroundColor = black
+        textAlign = TextAlign.left
+        borderWidth = 0.5.vmin
+        borderStyle = solid
+        borderColor = darkGrey
+        cursor = pointer
+        fontWeight = bold
+        fontSize = 3.vmin
+        paddingTop = 1.vmin
+        paddingBottom = 1.5.vmin
+        outline = Outline.none
+    }
+    "button.current" {
+        color = black
+        backgroundColor = darkGrey
+    }
+    "button:hover" {
+        color = black
+        backgroundColor = dimGrey
+    }
+    "button.current:hover" {
+        color = black
+        backgroundColor = Color("#c3c3c3")
+    }
+    "button.leftEdge" {
+        paddingRight = 3.vmin
+        paddingLeft = 2.vmin
+        borderTopLeftRadius = 0.vmin
+        borderTopRightRadius = 4.vmin
+        borderBottomRightRadius = 4.vmin
+        borderBottomLeftRadius = 0.vmin
+    }
+    "button.rightEdge" {
+        paddingTop = 1.vmin
+        paddingRight = 2.vmin
+        paddingBottom = 1.5.vmin
+        paddingLeft = 3.vmin
+        borderTopLeftRadius = 4.vmin
+        borderTopRightRadius = 0.vmin
+        borderBottomRightRadius = 0.vmin
+        borderBottomLeftRadius = 4.vmin
+    }
+    "button:active" {
+        color = black
+        backgroundColor = dimGrey
+        borderColor = dimGrey
+    }
+}
+
+private fun CSSBuilder.selectionDetailsCss() {
+    "#selection-details" {
+        position = fixed
+        right = 0.vmin
+        bottom = 0.vmin
+        zIndex = 10
+        display = grid
+        gridTemplateColumns = GridTemplateColumns("auto 10vmin")
+        gap = Gap("2vmin")
+        padding(3.vmin, 6.vmin)
+        color = lightGrey
+        backgroundColor = black
+        borderWidth = 0.5.vmin
+        borderStyle = solid
+        borderColor = darkGrey
+        borderTopLeftRadius = 4.vmin
+        fontWeight = bold
+        fontSize = 3.vmin
+    }
+    "#selection-details button" {
+        borderRadius = 4.vmin
+        declarations["justifySelf"] = "center"
+        paddingLeft = 4.vmin
+        paddingRight = 4.vmin
+    }
+    "#selection-details .designation" {
+        gridColumn = GridColumn("span 2")
+    }
+    "#selection-details .bearing" {
+        textAlign = TextAlign.right
+    }
+    "#selection-details .range" {
+        textAlign = TextAlign.right
+    }
+    "#selection-details .detailsScanButton" {
+        gridRow = GridRow("4")
+        gridColumn = GridColumn("1 / span 2")
+    }
+    "#selection-details .detailsDeleteButton" {
+        gridRow = GridRow("4")
+        gridColumn = GridColumn("1 / span 2")
+    }
+}
+
+private fun CSSBuilder.popupCss() {
+    ".popup" {
+        zIndex = 10
+        display = grid
+        gap = Gap("2vmin")
+        padding(3.vmin, 6.vmin)
+        color = lightGrey
+        backgroundColor = black
+        borderWidth = 0.5.vmin
+        borderStyle = solid
+        borderColor = darkGrey
+        borderRadius = 4.vmin
+        fontWeight = bold
+        fontSize = 4.vmin
+        justifyContent = JustifyContent.center
+    }
+    ".popup button" {
+        borderRadius = 4.vmin
+        paddingLeft = 4.vmin
+        paddingRight = 4.vmin
+    }
+}
+
+private suspend inline fun ApplicationCall.respondCss(builder: CSSBuilder.() -> Unit) =
+    respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
+
