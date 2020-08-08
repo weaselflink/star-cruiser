530433e63d47b761628a0f1c5415ca2e2308da6a
weaselflink <stefan@stefanbissell.de>
2020-08-08 15:44:09 +0200

    clean up simulator code

--- server/src/main/kotlin/de/stefanbissell/starcruiser/physics/Simulator.kt
+++ server/src/main/kotlin/de/stefanbissell/starcruiser/physics/Simulator.kt
@@ -4,6 +4,8 @@ import de.stefanbissell.starcruiser.GameTime
 import de.stefanbissell.starcruiser.ships.Ship
 import de.stefanbissell.starcruiser.ships.ShipTemplate
 import de.stefanbissell.starcruiser.ships.cruiserTemplate
+import de.stefanbissell.starcruiser.toDegrees
+import de.stefanbissell.starcruiser.twoDigits
 import java.time.Instant
 import kotlin.math.abs
 
@@ -11,10 +13,15 @@ class Simulator(
     val shipTemplate: ShipTemplate
 ) {
 
-    fun analyzeShip() {
-        SimulationRun().analyzeMaxSpeed()
-        SimulationRun().analyzeTurnStart()
-    }
+    fun analyzeShip() =
+        PerformanceAnalysis(
+            linearAccelerationData = analyzeLinearAcceleration(),
+            angularAccelerationData = analyzeAngularAcceleration()
+        )
+
+    private fun analyzeLinearAcceleration() = SimulationRun().analyzeLinearAcceleration()
+
+    private fun analyzeAngularAcceleration() = SimulationRun().analyzeAngularAcceleration()
 
     private inner class SimulationRun {
         val gameTime = GameTime(Instant.EPOCH)
@@ -25,52 +32,85 @@ class Simulator(
             physicsEngine.addShip(ship)
         }
 
-        fun analyzeMaxSpeed() {
+        fun analyzeLinearAcceleration(): LinearAccelerationData {
             ship.changeThrottle(100)
-            val final = tickUntil { before, after ->
-                abs(before.speed.length() - after.speed.length()) < 0.0000001
+            val points = tickUntil { before, after ->
+                abs(before.bodyParameters.speed.length() - after.bodyParameters.speed.length()) < 0.00001
             }
 
-            println("maxSpeed:")
-            println(final.speed.length())
-            println(final.position.y)
-            println(gameTime.current)
+            val absoluteMax = points.last().bodyParameters.speed.length()
+            val pointAt99 = points.first { it.bodyParameters.speed.length() > absoluteMax * 0.99 }
+            return LinearAccelerationData(
+                maxSpeed = pointAt99.bodyParameters.speed.length().twoDigits(),
+                timeToMax = pointAt99.time.twoDigits(),
+                distanceToMax = pointAt99.bodyParameters.position.y.twoDigits()
+            )
         }
 
-        fun analyzeTurnStart() {
+        fun analyzeAngularAcceleration(): AngularAccelerationData {
             ship.changeRudder(100)
-            val final = tickUntil { before, after ->
-                abs(before.rotationSpeed - after.rotationSpeed) < 0.0000001
+            val points = tickUntil { before, after ->
+                abs(before.bodyParameters.rotationSpeed - after.bodyParameters.rotationSpeed) < 0.00001
             }
 
-            println("turnStart:")
-            println(final.rotationSpeed)
-            println(final.rotation)
-            println(gameTime.current)
+            val absoluteMax = points.last().bodyParameters.rotationSpeed
+            val pointAt99 = points.first { it.bodyParameters.rotationSpeed > absoluteMax * 0.99 }
+            return AngularAccelerationData(
+                maxRotationSpeed = pointAt99.bodyParameters.rotationSpeed.toDegrees().twoDigits(),
+                timeToMax = pointAt99.time.twoDigits(),
+                rotationToMax = pointAt99.bodyParameters.rotation.toDegrees().twoDigits()
+            )
         }
 
-        private fun tickUntil(condition: (BodyParameters, BodyParameters) -> Boolean): BodyParameters {
+        private fun tickUntil(condition: (SimulationState, SimulationState) -> Boolean): List<SimulationState> {
+            val points = mutableListOf<SimulationState>()
             var (before, after) = tick()
+            points += before
             while (!condition(before, after)) {
                 tick().also {
                     before = it.first
                     after = it.second
                 }
+                points += after
             }
-            return after
+            return points
         }
 
-        private fun tick(): Pair<BodyParameters, BodyParameters> {
-            val before = physicsEngine.getBodyParameters(ship.id)!!
+        private fun tick(): Pair<SimulationState, SimulationState> {
+            val before = SimulationState(physicsEngine.getBodyParameters(ship.id)!!, gameTime.current)
             gameTime.update(0.01)
             ship.update(gameTime, physicsEngine) { null }
             physicsEngine.step(gameTime.delta)
-            val after = physicsEngine.getBodyParameters(ship.id)!!
+            val after = SimulationState(physicsEngine.getBodyParameters(ship.id)!!, gameTime.current)
             return before to after
         }
     }
 }
 
+data class SimulationState(
+    val bodyParameters: BodyParameters,
+    val time: Double
+)
+
+data class LinearAccelerationData(
+    val maxSpeed: Double,
+    val timeToMax: Double,
+    val distanceToMax: Double
+)
+
+data class AngularAccelerationData(
+    val maxRotationSpeed: Double,
+    val timeToMax: Double,
+    val rotationToMax: Double
+)
+
+data class PerformanceAnalysis(
+    val linearAccelerationData: LinearAccelerationData,
+    val angularAccelerationData: AngularAccelerationData
+)
+
 fun main() {
-    Simulator(cruiserTemplate).analyzeShip()
+    println(
+        Simulator(cruiserTemplate).analyzeShip()
+    )
 }
