e848128d0b4d19e3e0906a047e6cb4384657dfa7
weaselflink <stefan@stefanbissell.de>
2020-08-29 23:44:56 +0200

    make AI checks every second

--- server/src/main/kotlin/de/stefanbissell/starcruiser/ai/ShipAi.kt
+++ server/src/main/kotlin/de/stefanbissell/starcruiser/ai/ShipAi.kt
@@ -3,12 +3,19 @@ package de.stefanbissell.starcruiser.ai
 import de.stefanbissell.starcruiser.GameTime
 import de.stefanbissell.starcruiser.ships.NonPlayerShip
 
-class ShipAi {
+class ShipAi(
+    private val interval: Double = 1.0
+) {
+
+    private var lastCheck: Double = Double.MIN_VALUE
 
     fun update(time: GameTime, ship: NonPlayerShip) {
-        with(ship.shieldHandler) {
-            if (!up && activationAllowed()) {
-                up = true
+        if (time.current - lastCheck > interval) {
+            lastCheck = time.current
+            with(ship.shieldHandler) {
+                if (!up && activationAllowed()) {
+                    up = true
+                }
             }
         }
     }
