bfb7da20b2b64dc54e02cb06eb74068252a76251
weaselflink <stefan@stefanbissell.de>
2020-09-08 08:28:24 +0200

    clean code

--- client/src/main/kotlin/three/ThreeExtensions.kt
+++ client/src/main/kotlin/three/ThreeExtensions.kt
@@ -13,15 +13,4 @@ fun PerspectiveCamera.updateSize(width: Number, height: Number) {
     updateProjectionMatrix()
 }
 
-fun Object3D.debugPrint(layer: Int = 0) {
-    val prefix = (0 until layer).joinToString(separator = "") { "---- " }
-    println("${prefix}name: $name")
-    println(with(position) { "${prefix}position: $x,$y,$z" })
-    println(with(rotation) { "${prefix}rotation: $x,$y,$z" })
-    println(with(scale) { "${prefix}scale: $x,$y,$z" })
-    children.forEach {
-        it.debugPrint(layer + 1)
-    }
-}
-
 fun Vector3.set(v: de.stefanbissell.starcruiser.Vector3) = set(v.x, v.y, v.z)
