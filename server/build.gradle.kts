1bb29e9d0fdc96cde0c53bcc5925d1a49051d895
weaselflink <stefan@stefanbissell.de>
2020-10-01 07:02:29 +0200

    keep dependencies up to date

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
 
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.1.0")
-    implementation("org.jetbrains:kotlin-css:1.0.0-pre.122-kotlin-$kotlin_version")
+    implementation("org.jetbrains:kotlin-css:1.0.0-pre.123-kotlin-$kotlin_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-core:$ktor_version")
