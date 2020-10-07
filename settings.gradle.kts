98e2a8a7de4b68385ec51af70ec30a2c4aa44cee
weaselflink <stefan@stefanbissell.de>
2020-10-07 09:08:05 +0200

    keep dependencies up to date

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,9 +9,9 @@ pluginManagement {
         kotlin("jvm") version kotlin_version
         kotlin("js") version kotlin_version
         kotlin("plugin.serialization") version kotlin_version
-        id("com.github.johnrengelman.shadow") version "6.0.0"
+        id("com.github.johnrengelman.shadow") version "6.1.0"
         id("com.github.ben-manes.versions") version "0.33.0"
-        id("org.jlleitschuh.gradle.ktlint") version "9.4.0"
+        id("org.jlleitschuh.gradle.ktlint") version "9.4.1"
         id("com.adarshr.test-logger") version "2.1.0"
     }
 }
