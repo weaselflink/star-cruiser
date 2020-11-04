3f2ded5f64a39e0c2b7b17416ecbdcaf909848f5
weaselflink <stefan@stefanbissell.de>
2020-11-04 10:08:51 +0100

    clean code

--- server/src/main/kotlin/de/stefanbissell/starcruiser/client/StatisticsActor.kt
+++ server/src/main/kotlin/de/stefanbissell/starcruiser/client/StatisticsActor.kt
@@ -60,6 +60,13 @@ data class StatisticsSnapshot(
     val totalReceived: Long
 ) {
 
+    private val scales = listOf(
+        1024L * 1024L * 1024L to "GB",
+        1024L * 1024L to "MB",
+        1024L to "kB",
+        1L to "B"
+    )
+
     fun toJson() = configuredJson.encodeToString(serializer(), this)
 
     fun log() {
@@ -83,14 +90,5 @@ data class StatisticsSnapshot(
             }
         }
         return "0 B"
-    }
-
-    companion object {
-        private val scales = listOf(
-            1024L * 1024L * 1024L to "GB",
-            1024L * 1024L to "MB",
-            1024L to "kB",
-            1L to "B"
-        )
     }
 }
