4bc926dea69d4a7057f8bb06db1c5b87a949b454
weaselflink <stefan@stefanbissell.de>
2020-11-06 10:11:04 +0100

    use data driven tests

--- shared/src/jvmTest/kotlin/de/stefanbissell/starcruiser/NumberExtensionsTest.kt
+++ shared/src/jvmTest/kotlin/de/stefanbissell/starcruiser/NumberExtensionsTest.kt
@@ -1,6 +1,8 @@
 package de.stefanbissell.starcruiser
 
+import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestFactory
 import strikt.api.expectCatching
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
@@ -9,70 +11,112 @@ import kotlin.math.PI
 
 class NumberExtensionsTest {
 
-    @Test
-    fun `converts to radians`() {
-        expectThat(90.0.toRadians()).isNear(PI / 2)
-        expectThat((-90.0).toRadians()).isNear(-PI / 2)
-        expectThat(45.0.toRadians()).isNear(PI / 4)
-        expectThat(30.0.toRadians()).isNear(PI / 6)
-        expectThat(360.0.toRadians()).isNear(PI * 2)
-    }
+    @TestFactory
+    fun `converts to radians`(): List<DynamicTest> =
+        listOf(
+            90.0 to PI / 2,
+            -90.0 to -PI / 2,
+            45.0 to PI / 4,
+            30.0 to PI / 6,
+            360.0 to PI * 2
+        ).map { (degrees, radians) ->
+            DynamicTest.dynamicTest(
+                "$degrees degrees converts to $radians in radians"
+            ) {
+                expectThat(degrees.toRadians()).isNear(radians)
+            }
+        }
 
-    @Test
-    fun `converts to degrees`() {
-        expectThat((PI / 2).toDegrees()).isNear(90.0)
-        expectThat((-PI / 2).toDegrees()).isNear(-90.0)
-        expectThat((PI / 4).toDegrees()).isNear(45.0)
-        expectThat((PI / 6).toDegrees()).isNear(30.0)
-        expectThat((PI * 2).toDegrees()).isNear(360.0)
-    }
+    @TestFactory
+    fun `converts to degrees`(): List<DynamicTest> =
+        listOf(
+            PI / 2 to 90.0,
+            -PI / 2 to -90.0,
+            PI / 4 to 45.0,
+            PI / 6 to 30.0,
+            PI * 2 to 360.0
+        ).map { (radians, degrees) ->
+            DynamicTest.dynamicTest(
+                "$radians radians converts to $degrees in degrees"
+            ) {
+                expectThat(radians.toDegrees()).isNear(degrees)
+            }
+        }
 
-    @Test
-    fun `converts to heading`() {
-        expectThat(0.0.toHeading()).isNear(90.0)
-        expectThat((-PI / 2).toHeading()).isNear(180.0)
-        expectThat((PI / 4).toHeading()).isNear(45.0)
-        expectThat((-PI / 4).toHeading()).isNear(135.0)
-        expectThat((PI / 6).toHeading()).isNear(60.0)
-        expectThat((PI * 2).toHeading()).isNear(90.0)
-        expectThat((-PI).toHeading()).isNear(270.0)
-        expectThat((-PI * 3).toHeading()).isNear(270.0)
-        expectThat((PI * 3).toHeading()).isNear(270.0)
-        expectThat((PI * 3 - PI / 6).toHeading()).isNear(300.0)
-    }
+    @TestFactory
+    fun `converts to heading`(): List<DynamicTest> =
+        listOf(
+            0.0 to 90.0,
+            -PI / 2 to 180.0,
+            PI / 4 to 45.0,
+            -PI / 4 to 135.0,
+            PI / 6 to 60.0,
+            PI * 2 to 90.0,
+            -PI to 270.0,
+            -PI * 3 to 270.0,
+            PI * 3 to 270.0,
+            PI * 3 - PI / 6 to 300.0
+        ).map { (radians, heading) ->
+            DynamicTest.dynamicTest(
+                "$radians radians converts to heading of $heading"
+            ) {
+                expectThat(radians.toHeading()).isNear(heading)
+            }
+        }
 
-    @Test
-    fun `converts to rounded heading`() {
-        expectThat(0.49.toRadians().toIntHeading()).isEqualTo(90)
-        expectThat(0.51.toRadians().toIntHeading()).isEqualTo(89)
-        expectThat(89.51.toRadians().toIntHeading()).isEqualTo(0)
-        expectThat(90.49.toRadians().toIntHeading()).isEqualTo(0)
-        expectThat(90.51.toRadians().toIntHeading()).isEqualTo(359)
-    }
+    @TestFactory
+    fun `converts to rounded heading`(): List<DynamicTest> =
+        listOf(
+            0.49 to 90,
+            0.51 to 89,
+            89.51 to 0,
+            90.49 to 0,
+            90.51 to 359
+        ).map { (degrees, heading) ->
+            DynamicTest.dynamicTest(
+                "$degrees degrees converts to rounded heading of $heading"
+            ) {
+                expectThat(degrees.toRadians().toIntHeading()).isEqualTo(heading)
+            }
+        }
 
-    @Test
-    fun `rounds to given digits`() {
-        expectThat(PI.round(0)).isNear(3.0)
-        expectThat(PI.round(2)).isNear(3.14)
-        expectThat(PI.round(4)).isNear(3.1416)
-        expectThat((-PI).round(0)).isNear(-3.0)
-        expectThat((-PI).round(2)).isNear(-3.14)
-        expectThat((-PI).round(4)).isNear(-3.1416)
-        expectThat(0.0.round(0)).isNear(0.0)
-        expectThat(0.0.round(4)).isNear(0.0)
-    }
+    @TestFactory
+    fun `rounds to given digits`(): List<DynamicTest> =
+        listOf(
+            PI to 0 gives 3.0,
+            PI to 2 gives 3.14,
+            PI to 4 gives 3.1416,
+            -PI to 0 gives -3.0,
+            -PI to 2 gives -3.14,
+            -PI to 4 gives -3.1416,
+            0.0 to 0 gives 0.0,
+            0.0 to 4 gives 0.0
+        ).map { (unrounded, digits, rounded) ->
+            DynamicTest.dynamicTest(
+                "$unrounded rounded to $digits digits gives $rounded"
+            ) {
+                expectThat(unrounded.round(digits)).isNear(rounded)
+            }
+        }
 
-    @Test
-    fun `formats to given digits`() {
-        expectThat(23.0.format(2)).isEqualTo("23.00")
-        expectThat((-23.0).format(2)).isEqualTo("-23.00")
-        expectThat(23.1.format(2)).isEqualTo("23.10")
-        expectThat(23.12.format(2)).isEqualTo("23.12")
-        expectThat(23.125.format(2)).isEqualTo("23.13")
-        expectThat(23.12576.format(2)).isEqualTo("23.13")
-        expectThat(23.12576.format(4)).isEqualTo("23.1258")
-        expectThat(23.12576.format(0)).isEqualTo("23")
-    }
+    @TestFactory
+    fun `formats to given digits`(): List<DynamicTest> =
+        listOf(
+            23.0 to 2 gives "23.00",
+            -23.0 to 2 gives "-23.00",
+            23.1 to 2 gives "23.10",
+            23.12 to 2 gives "23.12",
+            23.125 to 2 gives "23.13",
+            23.12576 to 2 gives "23.13",
+            23.12576 to 4 gives "23.1258",
+            23.12576 to 0 gives "23"
+        ).map { (unrounded, digits, formatted) ->
+            DynamicTest.dynamicTest(
+                "$unrounded formatted with $digits digits gives $formatted"
+            ) {
+                expectThat(unrounded.format(digits)).isEqualTo(formatted)
+            }
+        }
 
     @Test
     fun `no padding if width greater than target`() {
@@ -95,13 +139,23 @@ class NumberExtensionsTest {
         expectThat(0.pad(3)).isEqualTo("000")
     }
 
-    @Test
-    fun `add thousands separators`() {
-        expectThat(1.formatThousands()).isEqualTo("1")
-        expectThat(567.formatThousands()).isEqualTo("567")
-        expectThat(4567.formatThousands()).isEqualTo("4,567")
-        expectThat(34567.formatThousands()).isEqualTo("34,567")
-        expectThat(234567.formatThousands()).isEqualTo("234,567")
-        expectThat(1234567.formatThousands()).isEqualTo("1,234,567")
-    }
+    @TestFactory
+    fun `add thousands separators`(): List<DynamicTest> =
+        listOf(
+            1 to "1",
+            567 to "567",
+            4567 to "4,567",
+            -4567 to "-4,567",
+            34567 to "34,567",
+            234567 to "234,567",
+            1234567 to "1,234,567"
+        ).map { (number, withThousands) ->
+            DynamicTest.dynamicTest(
+                "$number with thousands separator gives $withThousands"
+            ) {
+                expectThat(number.formatThousands()).isEqualTo(withThousands)
+            }
+        }
+
+    private infix fun <A, B, C> Pair<A, B>.gives(c: C) = Triple(first, second, c)
 }
