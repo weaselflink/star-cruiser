7845f56d24bbc50629209ed5cb767b62385f262c
weaselflink <stefan@stefanbissell.de>
2020-12-07 16:57:50 +0100

    add names to torpedo components

--- server/src/main/kotlin/de/stefanbissell/starcruiser/ships/combat/TorpedoTemplate.kt
+++ server/src/main/kotlin/de/stefanbissell/starcruiser/ships/combat/TorpedoTemplate.kt
@@ -12,11 +12,13 @@ data class TorpedoTemplate(
 }
 
 data class TorpedoWarheadTemplate(
+    val name: String = "Basic",
     val mass: Double = 50.0,
     val damage: Double = 4.0
 )
 
 data class TorpedoDriveTemplate(
+    val name: String = "Basic",
     val mass: Double = 50.0,
     val thrust: Double = 2_000.0,
     val maxBurnTime: Double = 20.0
