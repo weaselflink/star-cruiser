c72c75de58a1020d13ab90e38873112f304deef7
weaselflink <stefan@stefanbissell.de>
2021-07-26 19:41:43 +0200

    create release

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 
 allprojects {
     group = "de.stefanbissell.starcruiser"
-    version = "0.39.0"
+    version = "0.40.0"
 
     apply(plugin = "com.github.ben-manes.versions")
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
